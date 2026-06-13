51b48517602bdf7bda4f85a4f56f5cccb613a79e
Mabel Samuel <mapleberrysamuel@gmail.com>
2026-06-13 10:34:44 +0100

    solved mario

---
+++ problem_set_1/mario-less/mario.c
@@ -0,0 +1,30 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_row(int spaces, int bricks);
+int main(void)
+{
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    } while (height < 1);
+
+    for (int i = 0; i < height; i++)
+    {
+        print_row(height - i - 1, i + 1);
+    }
+}
+
+void print_row(int spaces, int bricks)
+{
+    for (int i = 0; i < spaces; i++)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j < bricks; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
